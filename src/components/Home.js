e576ee1267b078cfd61bb6f1a9dcf57def1a3868
Shazeen Fabius <shazeenfabius@Shazeens-Pro.lan>
2020-11-20 00:32:59 -0800

    adding links to my socials and email

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <div className="home">
-      <p>All There Is To Know About Me</p>
+      <a href="https://github.com/Shazeen15" target='_blank' rel='noreferrer'>GitHub</a>
+      <a href="https://www.linkedin.com/in/shazeen-fabius-52635b135/" target='_blank' rel='noreferrer'>LinkedIn</a>
+      <a href="mailto:shazeenfabius@gmail.com">eMail</a>
     </div>
   );
 }
